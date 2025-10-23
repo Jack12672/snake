51998a3d5bd37a897abb371a9442067579f53201
Jack12672 <jagoire@ymail.com>
2025-10-24 00:13:37 +0200

    snake fonctionnel

--- snake v0.py
+++ snake v0.py
@@ -7,21 +7,22 @@ CASE=20
 
 class Snake():
     def __init__(self,dimension):
-        self.isdead=False
-        self.dx=1
-        self.dy=0
+        self.isalive=True
+        self.dx=0
+        self.dy=1
         self.position=[]
         self.length=3
+        self.fruit=(0,0)
         x1=int(dimension[0]/2)
         y1=int(dimension[1]/2)
+        x1=1
+        y1=1
         self.head=((x1,y1))
         self.position.append(self.head)
         y1+=1
         self.position.append((x1,y1))
         x1-=1
         self.position.append((x1,y1))
-        self.fruit=(y1-3,x1+2)
-        self.count=0
 
 
 class Grid():
@@ -39,24 +40,11 @@ class Grid():
 class Can(Canvas):
     def __init__(self,canvas,dimension):
         self.dimension=dimension
-        super().__init__(canvas,bg="white",width=dimension[0]*CASE,height=dimension[1]*CASE)
         self.obj=[]
         self.fruit=0
         self.grid=Grid(dimension)
         self.snake=Snake(dimension)
-        self.init_grid()
-        
-
-
-    def init_grid(self):
-        for i in range (self.snake.length):
-            x,y=self.snake.position[i]
-            self.grid.grid[x][y]=1
-            x,y=x*CASE,y*CASE
-            self.obj.append(self.create_oval(x,y,x+CASE,y+CASE,width = 1, fill="green"))
-        self.draw_fruit()
-        self.draw_grid
-        self.pack()
+        super().__init__(canvas,bg="white",width=dimension[0]*CASE,height=dimension[1]*CASE)
 
     def draw_grid(self):      
         color='blue'
@@ -68,36 +56,17 @@ class Can(Canvas):
             self.create_line(0,y,w,y,width = 1, fill=color)
         self.pack()
 
-    def draw_snake(self,growth):
-        if growth!=True:
-            l=self.snake.length
-            x,y=self.snake.position[l-1][0],self.snake.position[l-1][1]
-            self.grid.grid[x][y]=0
-        for i in range (self.snake.length-2,-1,-1):
+    def draw_snake(self):
+        for i in range (self.snake.length):
             x,y=self.snake.position[i][0],self.snake.position[i][1]
-            x1,y1=self.snake.position[i+1][0],self.snake.position[i+1][1]
-            self.snake.position[i+1]=(x,y)
-            self.move(self.obj[i+1],(x-x1)*CASE,(y-y1)*CASE)
-        x,y=self.snake.head
-        x1,y1=self.snake.position[0][0],self.snake.position[0][1]
-        self.snake.position[0]=(x,y)
-        self.grid.grid[x][y]=1
-        self.move(self.obj[0],(x-x1)*CASE,(y-y1)*CASE)
-        if self.snake.count>15:
-            self.snake.count-=1
-            self.itemconfigure(self.obj[0],fill='cyan')
-        elif self.snake.count>10:
-            self.snake.count-=1
-            self.itemconfigure(self.obj[0],fill='deep sky blue')
-        elif self.snake.count>5:
-            self.snake.count-=1
-            self.itemconfigure(self.obj[0],fill='DodgerBlue2')
-        elif self.snake.count>0:
-            self.itemconfigure(self.obj[0],fill='green')
-
+            x0,y0=x*CASE,y*CASE
+            x1,y1=x0+CASE,y0+CASE
+            self.obj.append(self.create_oval(x0,y0,x1,y1,width = 1, fill="green"))
+            self.grid.grid[x][y]=1
         self.pack()
 
     def draw_fruit(self):
+        
         maxx,maxy=self.dimension
         isvalid=True
         x,y=0,0     
@@ -115,47 +84,54 @@ class Can(Canvas):
         x1,y1=x0+CASE,y0+CASE
         self.fruit=self.create_oval(x0,y0,x1,y1,width = 1, fill="red")
 
-    def update(self):
-            x,y=self.snake.position[0][0]+self.snake.dx,self.snake.position[0][1]+self.snake.dy
-            self.snake.head=(x,y)
-            growth=False
-            if self.check_obstacle():
-                print('end')
-            if self.check_fruit():
-                growth=True
-            if self.snake.isdead==False:
-                self.draw_snake(growth)
 
+    def update (self):
+        l=self.snake.length
+        for i in range (self.snake.length-2,-1,-1):
+            x,y=self.snake.position[i][0],self.snake.position[i][1]
+            x1,y1=self.snake.position[i+1][0],self.snake.position[i+1][1]
+            self.snake.position[i+1]=(x,y)
+            self.move(self.obj[i+1],(x-x1)*CASE,(y-y1)*CASE)
+
+        x,y=self.snake.position[0][0]+self.snake.dx,self.snake.position[0][1]+self.snake.dy
+        self.snake.head=(x,y)
+        if self.check_obstacle() == False:   
+            self.snake.position[0]=(x,y)
+            self.grid.grid[x][y]=1
+            self.move(self.obj[0],self.snake.dx*CASE,self.snake.dy*CASE)
+
+        if self.check_fruit():
+            x,y=self.snake.position[l-1][0],self.snake.position[l-1][1]
+            self.snake.position.append((x,y))
+            self.obj.append(self.create_oval(x*CASE,y*CASE,x*CASE+CASE,y*CASE+CASE,width = 1, fill="green"))
+            self.snake.length+=1
+            self.delete(self.fruit)
+            self.draw_fruit()
+        else: 
+            x,y=self.snake.position[l-1][0],self.snake.position[l-1][1]
+            self.grid.grid[x][y]=0           
+        self.pack()
+        
 
     def check_obstacle (self):
         x,y = self.snake.head
         res=False
         if x<0 or x>self.grid.dimension[0]-1 : 
-            self.snake.isdead=True
+            self.snake.isalive=False
             res=True
         if y<0 or y>self.grid.dimension[1]-1: 
-            self.snake.isdead=True
+            self.snake.isalive=False
             res=True
-        if self.snake.isdead==False and self.grid.grid[x][y]!=0:
+        if self.grid.grid[x][y]!=0:
             res=True
-            self.snake.isdead=True
+            self.snake.isalive=False
         return res
 
 
     def check_fruit(self):
         x,y = self.snake.head
         xf,yf = self.snake.fruit
-        if (x==xf and y==yf): 
-            self.delete(self.fruit)
-            l=self.snake.length
-            x,y=self.snake.position[l-1][0],self.snake.position[l-1][1]
-            self.snake.position.append((x,y))
-            x,y=x*CASE,y*CASE
-            self.obj.append(self.create_oval(x,y,x+CASE,y+CASE,width = 1, fill="green"))
-            self.snake.length+=1
-            self.snake.count=20
-            self.draw_fruit()
-            return True
+        if (x==xf and y==yf): return True
         else: return False
 
 
@@ -184,40 +160,38 @@ class Window_0(Frame):
         self.master.bind("<Button-3>",lambda event: self.mousedown_right(event))
         self.master.bind("<B1-Motion>",lambda event: self.move_mouse(event))
         # child=Windows(bg='green',width=300,height=300 )
-
+        self.w.draw_grid()
+        self.w.draw_snake()
+        self.w.draw_fruit()
 
     def left (self,event):
-        if self.w.snake.dx!=1:
-            self.w.snake.dx=-1
-            self.w.snake.dy=0
+        self.w.snake.dx=-1
+        self.w.snake.dy=0
 
     def right (self,event):
-        if self.w.snake.dx!=-1:
-            self.w.snake.dx=1
-            self.w.snake.dy=0
+        self.w.snake.dx=1
+        self.w.snake.dy=0
         
     def up (self,event):
-        if self.w.snake.dy!=1:
-            self.w.snake.dy=-1
-            self.w.snake.dx=0
+        self.w.snake.dy=-1
+        self.w.snake.dx=0
         
     def down (self,event):
-        if self.w.snake.dy!=-1:
-            self.w.snake.dy=1
-            self.w.snake.dx=0
+        self.w.snake.dy=1
+        self.w.snake.dx=0
 
     def move_mouse (self,event):
         pass
     
     def mousedown_left(self, event):
-        self.w.snake.isdead=False
+        self.w.snake.isalive=True
         self.start()
 
     def mouseup_left(self, event):
         pass
         
     def mousedown_scroll_wheel(self, event):
-        self.w.snake.isdead=True
+        self.w.snake.isalive=False
         for y in range (30):
             l="|"
             for x in range (30):
@@ -232,7 +206,10 @@ class Window_0(Frame):
         self.w.update()
 
     def start(self):
-        if self.w.snake.isdead==False:
+        pass
+
+    def start(self):
+        if self.w.snake.isalive:
             self.w.update()
             self.w.after(SPEED,self.start)
 
